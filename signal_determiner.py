99b4aec5290c0771edd640cc276e0db365a651c9
James H. <lsaucyfx@gmail.com>
2024-02-19 17:31:13 -0500

    add Signal Determiner class

--- signal_determiner.py
+++ signal_determiner.py
@@ -3,5 +3,10 @@ import numpy as np
 
 class SignalDeterminer:
     def determine_signals(self, df):
-        df['Signal'] = np.where(df['close'] > df['SMA_High'].shift(1),)
-                                   
+        df['Signal'] = np.where(df['close'] > df['SMA_High'].shift(1), 1,np.where(df['close'], df['SMA_Low'].shift(1), -1, 0))
+        df['BullishCondition'] = (df['close'] > df['SMA_High'].shift(1)) & (df['ADX'] > 50)
+        df['BearishCondition'] = (df['close'] < df['SMA_Low'].shift(1)) &  (df['ADX'] > 50)
+        df['BuySignal'] = np.where((df['Signal'] == 1) & df['BullishCondition'], 1, 0)
+        df['SellSignal'] = np.where((df)['Signal'] == -1 | (df['close'] < df['TrailingStop']) , -1, 0)
+
+        return df
