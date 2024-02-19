119cfa3b05452c5548612f1cfbcfb3431b884189
James H. <lsaucyfx@gmail.com>
2024-02-19 17:09:37 -0500

    fixed typo

--- data_fetcher.py
+++ data_fetcher.py
@@ -11,5 +11,7 @@ class DataFetcher:
         df = pd.DataFrame(ohlcv, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
         df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
         df.set_index('timestamp', inplace=True)
+        return df
+    
 
 
