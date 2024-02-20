1d07c878eac1c9b1ac3a9d36c7c04d4939cddace
Lance H <lsaucyfx@gmail.com>
2024-02-20 07:00:15 -0500

    updated backtestsr class

--- server/Backtester.py
+++ server/Backtester.py
@@ -3,22 +3,25 @@ import numpy as np
 import composite_strategy, data_fetcher, indicator_calculator
 import signal_determiner, signal_printer
 class Backtester:
-    def __init__(self, strategy):
-        strategy = signal_determiner()
+    def __init__(self, data_fetcher, indicator_calculator, signal_determiner, signal_printer):
+        strategy = signal_determiner.determine_signals()
+        data = 
         self.strategy = strategy
+
 
     def run_backtest(self, historical_data):
        
         # create a copy of the data to avoid modifying the original
+        historical_data = data_fetcher()
         df = pd.DataFrame(historical_data.copy(),columns=['timestamp', 'open', 'high','low', 'close', 'volume'])
         df['timestamp'] = pd.to_datetime(df['timestamp'], unit ='ms')
         df.set_index('timestamp', inplace=True)
 
         # calculate indicators using the trading strategy
-        df = self.strategy.indicator_calculator(df)
+        df = self.indicator_calculator(df)
 
         # apply the trading strategy to determine signals
-        df = self.strategy.signal_determiner(df)
+        df = self.strategy.signal_determiner.determine_signals(df)
 
         # simulate trading based on signals
         self.simulate_trading(df)
